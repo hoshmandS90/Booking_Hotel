a0ad19df6c3ca5461a332df7e3cdf7a4e111b6cf
HoshmandG90 <hoshmandG900@gmail.com>
2022-01-29 21:50:45 +0300

    added Dining Page

--- resources/views/dining.blade.php
+++ resources/views/dining.blade.php
@@ -1,4 +1,12 @@
 <div>
 
-    Hi Dear
+<div class="w-full h-screen bg-center bg-no-repeat bg-cover" style="background-image: url('/assets/img/rest.jpg');">
+    <div class="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center">
+        <div class="mx-4 text-center text-white">
+            <h1 class="font-bold text-6xl mb-4 capitalize font-semibold">The dining room</h1>
+      
+        </div>
+    </div>
+</div>
+
 </div>
